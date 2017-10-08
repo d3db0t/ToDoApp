fa48c63e5cb0ceea5fab3f916ba168dabfac077a
d3db0t <ShadyTantawy@protonmail.com>
2017-10-08 13:39:53 +0200

    [+] Added welcome view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<html>
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4"></script>
+        
+    <body>
+        <div class="jumbotron" align="center">
+            <h1>ToDoApp</h1>
+            <p>This is a simple todolist Web App.</p>
+            <p><a class="btn btn-primary btn-lg" href="/login">Start</a></p>
+        </div>   
+
+    </body>
+</html>
