cfabd94c18e960b981e6ca9da56ec0fde2acbc5d
d3db0t <ShadyTantawy@protonmail.com>
2017-10-08 12:48:00 +0200

    [+] Added Views
    -Changed from html to blade.php
    -Deleted default laravel welcome view

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,94 @@
+<html>
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4"></script>
+        
+    <body>
+            <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                      <div class="navbar-header">
+                        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">ToDoList</a>
+                      </div>
+                  
+                      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                          <li class="active"><a href="#">Dashboard <span class="sr-only">(current)</span></a></li>
+                          <li><a href="addTask.html">AddTask</a></li>
+                          
+                        </ul>
+                        <form class="navbar-form navbar-left" role="search">
+                          <div class="form-group">
+                            <input class="form-control" type="text" placeholder="Search">
+                          </div>
+                          <button class="btn btn-default" type="submit">Submit</button>
+                        </form>
+                        <ul class="nav navbar-nav navbar-right">
+                          <li><a href="#">Logout</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </nav>
+                  <!--FakeTasks-->
+                  <table class="table">
+                        <thead>
+                          <tr class="success">
+                            <th>#</th>
+                            <th>Tasks</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>1</td>
+                            <td>Task1 bla bla bla hahahahahahhaah</td>
+                            <td><button class="btn btn-default" type="submit">Edit</button></td>
+                            <td><button class="btn btn-danger" type="submit">Delete</button></td>
+                          </tr>
+                          <tr>
+                            <td>2</td>
+                            <td>Task2</td>
+                            <td><button class="btn btn-default" type="submit">Edit</button></td>
+                            <td><button class="btn btn-danger" type="submit">Delete</button></td>
+                          </tr>
+                          <tr>
+                            <td>3</td>
+                            <td>Task3</td>
+                            <td><button class="btn btn-default" type="submit">Edit</button></td>
+                            <td><button class="btn btn-danger" type="submit">Delete</button></td>
+                          </tr>
+                          <tr>
+                            <td>4</td>
+                            <td>Task4</td>
+                            <td><button class="btn btn-default" type="submit">Edit</button></td>
+                            <td><button class="btn btn-danger" type="submit">Delete</button></td>
+                          </tr>
+                          <tr>
+                            <td>5</td>
+                            <td>Task5</td>
+                            <td><button class="btn btn-default" type="submit">Edit</button></td>
+                            <td><button class="btn btn-danger" type="submit">Delete</button></td>
+                          </tr>
+                          <tr>
+                            <td>6</td>
+                            <td>Task6</td>
+                            <td><button class="btn btn-default" type="submit">Edit</button></td>
+                            <td><button class="btn btn-danger" type="submit">Delete</button></td>
+                          </tr>
+                          <tr>
+                            <td>7</td>
+                            <td>Task7</td>
+                            <td><button class="btn btn-default" type="submit">Edit</button></td>
+                            <td><button class="btn btn-danger" type="submit">Delete</button></td>
+                          </tr>
+                        </tbody>
+                      </table>      
+
+    </body>
+</html>
---
+++ resources/views/editTask.blade.php
@@ -0,0 +1,54 @@
+<html>
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4"></script>
+        
+    <body>
+            <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                      <div class="navbar-header">
+                        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">ToDoList</a>
+                      </div>
+                  
+                      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                          <li class="active"><a href="dashboard.html">Dashboard <span class="sr-only">(current)</span></a></li>
+                          <li><a href="addTask.html">AddTask</a></li>
+                          
+                        </ul>
+                        <form class="navbar-form navbar-left" role="search">
+                          <div class="form-group">
+                            <input class="form-control" type="text" placeholder="Search">
+                          </div>
+                          <button class="btn btn-default" type="submit">Submit</button>
+                        </form>
+                        <ul class="nav navbar-nav navbar-right">
+                          <li><a href="#">Logout</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </nav>
+                  <div class="form-group">
+                        <label class="col-lg-2 control-label" for="textArea">Edit Task</label>
+                        <div class="col-lg-10">
+                          <textarea class="form-control" id="textArea" rows="3"></textarea>
+                          <span class="help-block">Edit the content of the task.</span>
+                        </div>
+                  <div class="form-group">
+                        <div class="col-lg-10 col-lg-offset-2">
+                            <button class="btn btn-default" type="reset">Cancel</button>
+                            <button class="btn btn-primary" type="submit">Edit</button>
+                        </div>
+                  </div>     
+                             
+
+    </body>
+</html>
