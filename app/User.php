da1805cec2fcb69ee51ddb9e9542cbda510ae056
d3db0t <ShadyTantawy@protonmail.com>
2017-10-11 16:08:53 +0200

    [+] Added User relation

--- app/User.php
+++ app/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    # User can have many tasks
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
