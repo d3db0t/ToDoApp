938484dfbb68337f8e2f0b6458aa1daeb47962dc
d3db0t <ShadyTantawy@protonmail.com>
2017-10-11 16:17:11 +0200

    [+] Added Task relation

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    # Every Task belongsto one User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
